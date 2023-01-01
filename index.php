0d6b9b360cacad6b458ed5c91ca59ee542869060
eaker <eakermane@gmail.com>
2023-01-02 00:43:31 +0200

    kaut kas ir

--- index.php
+++ index.php
@@ -10,12 +10,16 @@
     <div class="header">
         <a href="#default" class="logo">Zvaigznīte</a>
         <div class="header-right">
-            <a>Grupa 1</a>
-            <a>Grupa 2</a>
-            <a>Grupa 3</a>
-            <a>Grupa 4</a>
-            <a>Grupa 5</a>
-            <a>Grupa 6</a>
+            <div class="dropdown">
+                <button class="dropbtn">Grupas
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="#">Zvaniņi</a>
+                    <a href="#">Bitītes</a>
+                    <a href="#">Kumelītes</a>
+                </div>
+            </div>
             <a>Jaunumi</a>
             <a href="foto.php">Foto Galerija</a>
             <a>Kalendārs</a>
@@ -23,12 +27,22 @@
             <a href="log_in.php">Pieslēgties</a>
         </div>
     </div>
-   
-    <form method="post" action="process.php">
-  Enter name: <input type="text" name="fname">
-  <input type="submit">
-</form>
 
+    <div class="float-container">
+        <div class="float-child-homeDiv">
+            <h1>Par mums</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec laoreet iaculis rutrum. Etiam elementum vitae nisl a vestibulum. Nulla egestas est arcu, nec sodales odio semper vitae. Maecenas ultrices rhoncus nibh eu molestie. Sed dictum sapien a arcu porttitor, ut ornare neque tristique. Ut mattis dolor at lacus efficitur vestibulum. Sed sagittis sollicitudin nisl, nec pellentesque turpis ullamcorper luctus. Integer sem tellus, scelerisque et facilisis lacinia, volutpat quis urna. Nulla sed lacus pulvinar, porta ex at, porta nunc. Cras sodales gravida congue. In hac habitasse platea dictumst. Proin leo mauris, sodales in sodales eget, rhoncus ut augue. Cras maximus nibh vel enim scelerisque, nec hendrerit risus facilisis. Nunc at vulputate lorem. Aliquam erat volutpat. Sed pulvinar tempor dolor, sit amet dignissim lacus viverra id. </p>
+            <p>In feugiat metus non cursus gravida. In consequat, diam sed sodales tristique, eros dui gravida est, quis rutrum sem augue sollicitudin felis. Curabitur maximus condimentum iaculis. Nam tristique ligula vitae eleifend sagittis. Duis vitae mattis elit. Suspendisse pharetra posuere turpis eu dignissim. Phasellus vulputate massa vitae urna pharetra placerat. Fusce eget odio id tortor ultricies sagittis ut non ex. Suspendisse nec elementum quam, a varius leo. Ut et ultricies turpis. Sed quis facilisis libero. Vivamus at efficitur magna. Mauris in metus vitae lorem maximus sagittis ut sit amet ipsum. Duis lobortis faucibus orci.</p>
+        </div>
+        <div class="float-child-homeDiv2">
+            <h1>Ikdiena</h1>
+            <ul>
+                <li>Coffee</li>
+                <li>Tea</li>
+                <li>Milk</li>
+            </ul>
+        </div>
+    </div>
 
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
