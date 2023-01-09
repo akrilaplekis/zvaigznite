17c0a76733081cbf497e07be8feea57e70eaab43
eaker <eakermane@gmail.com>
2023-01-09 21:19:31 +0200

    user_page start

---
+++ user_page.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Zvaigznīte</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="index.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body style="background-color: #ffdb57;">
+<nav class="navbar navbar-custom navbar-fixed-to">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="index.php">Zvaigznīte</a>
+        </div>
+        <ul class="nav navbar-nav navbar-right">
+            <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Grupas<span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="grupas.php?id=1">Bitītes</a></li>
+                    <li><a href="grupas.php?id=2">Pienenes</a></li>
+                    <li><a href="grupas.php?id=3">Kumelītes</a></li>
+                    <li><a href="grupas.php?id=4">Saulespuķes</a></li>
+                    <li><a href="grupas.php?id=5">Zvaniņi</a></li>
+                    <li><a href="grupas.php?id=6">Rudzupuķes</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Vecākiem</a></li>
+            <li><a href="foto.php">Foto Galerija</a></li>
+            <li><a href="kontakti.html">Kontakti</a></li>
+            <li><a href="log_in.php">Pieslēgties</a></li>
+        </ul>
+    </div>
+</nav>
+
+</body>
+</html>
